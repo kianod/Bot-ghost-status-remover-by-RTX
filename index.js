f643f6aed9e89d8d9a4e6811388204164a8fa8ca
kianod <starmarket@t-online.de>
2024-12-04 18:32:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = [".gg/supermarketreal",".gg/supermarketreal",".gg/supermarketreal"];
 
 
 let currentIndex = 0;
